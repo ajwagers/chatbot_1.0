ee8a21b00b5c841802d66efc249b624bcea140aa
Andy J. Wagers <44385569+ajwagers@users.noreply.github.com>
2023-04-10 22:39:23 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,85 @@
 '''
 This file will contain the training portion of the rnn word chatbot.
 '''
+
+import tensorflow as tf
+from utils import TextLoader
+from model import Model
+
+def train(args):
+   data_loader = TextLoader(args.data_dir, args.batch_size, args.seq_length)
+    args.vocab_size = data_loader.vocab_size
+
+    # check compatibility if training is continued from previously saved model
+    if args.init_from is not None:
+        # check if all necessary files exist
+        assert os.path.isdir(args.init_from)," %s must be a a path" % args.init_from
+        assert os.path.isfile(os.path.join(args.init_from,"config.pkl")),"config.pkl file does not exist in path %s"%args.init_from
+        assert os.path.isfile(os.path.join(args.init_from,"chars_vocab.pkl")),"chars_vocab.pkl.pkl file does not exist in path %s" % args.init_from
+        ckpt = tf.train.latest_checkpoint(args.init_from)
+        assert ckpt, "No checkpoint found"
+
+        # open old config and check if models are compatible
+        with open(os.path.join(args.init_from, 'config.pkl'), 'rb') as f:
+            saved_model_args = cPickle.load(f)
+        need_be_same = ["model", "rnn_size", "num_layers", "seq_length"]
+        for checkme in need_be_same:
+            assert vars(saved_model_args)[checkme]==vars(args)[checkme],"Command line argument and saved model disagree on '%s' "%checkme
+
+        # open saved vocab/dict and check if vocabs/dicts are compatible
+        with open(os.path.join(args.init_from, 'chars_vocab.pkl'), 'rb') as f:
+            saved_chars, saved_vocab = cPickle.load(f)
+        assert saved_chars==data_loader.chars, "Data and loaded model disagree on character set!"
+        assert saved_vocab==data_loader.vocab, "Data and loaded model disagree on dictionary mappings!"
+
+    if not os.path.isdir(args.save_dir):
+        os.makedirs(args.save_dir)
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'wb') as f:
+        cPickle.dump(args, f)
+    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'wb') as f:
+        cPickle.dump((data_loader.chars, data_loader.vocab), f)
+
+    model = Model(args)
+
+    with tf.Session() as sess:
+        # instrument for tensorboard
+        summaries = tf.summary.merge_all()
+        writer = tf.summary.FileWriter(
+                os.path.join(args.log_dir, time.strftime("%Y-%m-%d-%H-%M-%S")))
+        writer.add_graph(sess.graph)
+
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver(tf.global_variables())
+        # restore model
+        if args.init_from is not None:
+            saver.restore(sess, ckpt)
+        for e in range(args.num_epochs):
+            sess.run(tf.assign(model.lr,
+                               args.learning_rate * (args.decay_rate ** e)))
+            data_loader.reset_batch_pointer()
+            state = sess.run(model.initial_state)
+            for b in range(data_loader.num_batches):
+                start = time.time()
+                x, y = data_loader.next_batch()
+                feed = {model.input_data: x, model.targets: y}
+                for i, (c, h) in enumerate(model.initial_state):
+                    feed[c] = state[i].c
+                    feed[h] = state[i].h
+
+                # instrument for tensorboard
+                summ, train_loss, state, _ = sess.run([summaries, model.cost, model.final_state, model.train_op], feed)
+                writer.add_summary(summ, e * data_loader.num_batches + b)
+
+                end = time.time()
+                print("{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}"
+                      .format(e * data_loader.num_batches + b,
+                              args.num_epochs * data_loader.num_batches,
+                              e, train_loss, end - start))
+                if (e * data_loader.num_batches + b) % args.save_every == 0\
+                        or (e == args.num_epochs-1 and
+                            b == data_loader.num_batches-1):
+                    # save for the last result
+                    checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
+                    saver.save(sess, checkpoint_path,
+                               global_step=e * data_loader.num_batches + b)
+                    print("model saved to {}".format(checkpoint_path))
