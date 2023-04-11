feb48007c21f13fa3b5709e293e1f2c39c6ec4d8
Andy J. Wagers <44385569+ajwagers@users.noreply.github.com>
2023-04-10 22:40:23 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,47 @@
 '''
 This will contain the python code related to the model of the rnn word chatbot.
 '''
+
+import tensorflow as tf
+from tensorflow.contrib import rnn
+from tensorflow.contrib import legacy_seq2seq
+
+import numpy as np
+
+class Model():
+  def sample(self, sess, chars, vocab, num=200, prime='The ', sampling_type=1):
+        state = sess.run(self.cell.zero_state(1, tf.float32))
+        for char in prime[:-1]:
+            x = np.zeros((1, 1))
+            x[0, 0] = vocab[char]
+            feed = {self.input_data: x, self.initial_state: state}
+            [state] = sess.run([self.final_state], feed)
+
+        def weighted_pick(weights):
+            t = np.cumsum(weights)
+            s = np.sum(weights)
+            return(int(np.searchsorted(t, np.random.rand(1)*s)))
+
+        ret = prime
+        char = prime[-1]
+        for _ in range(num):
+            x = np.zeros((1, 1))
+            x[0, 0] = vocab[char]
+            feed = {self.input_data: x, self.initial_state: state}
+            [probs, state] = sess.run([self.probs, self.final_state], feed)
+            p = probs[0]
+
+            if sampling_type == 0:
+                sample = np.argmax(p)
+            elif sampling_type == 2:
+                if char == ' ':
+                    sample = weighted_pick(p)
+                else:
+                    sample = np.argmax(p)
+            else:  # sampling_type == 1 default:
+                sample = weighted_pick(p)
+
+            pred = chars[sample]
+            ret += pred
+            char = pred
+        return ret
